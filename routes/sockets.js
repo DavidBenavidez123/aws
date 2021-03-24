fbaf0435af43ad8257e43978cd7b6bbcce0e7629
David Benavidez <benavidez_david13@yahoo.com>
2021-03-24 00:35:59 -0700

    changed emit message

--- routes/sockets.js
+++ routes/sockets.js
@@ -3,7 +3,7 @@ module.exports = function (io) {
     io.on('connection', function (socket) {
         
         console.log('connected')
-        socket.emit('connection', socket.id);
+        socket.emit('connection', 'connected to server socket');
 
         
 
